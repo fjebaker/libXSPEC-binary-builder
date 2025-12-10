2ef8a0e3080100d5c4a80e1c6eb5039814ae4d35
fjebaker <fergus@cosroe.com>
2025-12-10 11:37:07 +0000

    feat: reltrans binary builder wrapper

---
+++ extra/LibXSPEC_Reltrans/build_tarballs.jl
@@ -0,0 +1,59 @@
+using BinaryBuilder
+
+name = "LibXSPEC_Reltrans"
+
+version = v"2.3.1"
+
+sources = [
+    GitSource(
+        "https://github.com/fjebaker/reltrans/",
+        "48f1c16d2a475d264057c4db2d3415fa05937aef",
+    ),
+    # TODO: add data sources? else make them dynamically fetched (probably better)
+]
+
+scripts = raw"""
+cd ${WORKSPACE}/srcdir
+
+export HEADAS="${prefix}"
+
+cd utils
+make
+
+mkdir -p ${prefix}/lib
+mv build/lib/* ${prefix}/lib/
+
+install_license ../LICENSE
+"""
+
+platforms = [
+    Platform("x86_64", "linux"; libc = "glibc", libgfortran_version = "5.0.0"),
+    # Platform("x86_64", "linux"; libc = "glibc", libgfortran_version = "4.0.0"),
+    # Platform("aarch64", "macos"; libgfortran_version = "5.0.0"),
+    # Platform("x86_64", "macos"; libgfortran_version = "5.0.0"),
+    # Platform("x86_64", "macos"; libgfortran_version = "4.0.0"),
+]
+# platforms = expand_cxxstring_abis(platforms)
+# platforms = expand_gfortran_versions(platforms)
+
+products = [LibraryProduct("libreltrans", :libXSPEC_reltrans)]
+
+dependencies = [Dependency("CompilerSupportLibraries_jll"), Dependency("LibXSPEC_jll")]
+
+init_block = """# set environment variable needed by the models
+    # TODO: fixme, check for the right environment variables regarding data tables,
+    # else print helpful error
+"""
+
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    scripts,
+    platforms,
+    products,
+    dependencies;
+    julia_compat = "1.6",
+    init_block = init_block,
+)
